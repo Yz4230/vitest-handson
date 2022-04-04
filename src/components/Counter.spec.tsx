8c894f7b277e59f48f17878c5469cddb99a07308
Yz4230 <yuzupon1480@gmail.com>
2022-04-04 13:37:49 +0900

    upgrade @testing-library/react: 12 -> 13

--- src/components/Counter.spec.tsx
+++ src/components/Counter.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import Counter from "./Counter";
 
@@ -6,9 +6,9 @@ describe("Counter", () => {
   it("should increment the count", () => {
     const { getByText } = render(<Counter />);
     const button = getByText(/I was clicked 0 times/);
-    button.click();
+    fireEvent.click(button);
     expect(getByText(/I was clicked 1 times/)).toBeInTheDocument();
-    button.click();
+    fireEvent.click(button);
     expect(getByText(/I was clicked 2 times/)).toBeInTheDocument();
   });
   it("snapshot", () => {
--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,3 +1,8 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 import "jest-extended";
+
+// configuration for React 18
+// see: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).IS_REACT_ACT_ENVIRONMENT = true;
